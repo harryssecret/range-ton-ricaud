d326614ab7098353fa4b8f155d3e161f6de8fce4
kity <geekcat@protonmail.com>
2023-05-20 14:28:54 +0200

    still trying to fix Connection string problem

--- range-ton-ricaud/Program.cs
+++ range-ton-ricaud/Program.cs
@@ -5,12 +5,7 @@ using range_ton_ricaud.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-if (connectionString == null)
-{
-    connectionString = Environment.GetEnvironmentVariable("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-}
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
